81b9a20133b13c62363d08af102115ce7738ec39
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2026-03-26 16:19:38 +0100

    Remove progress of curl download when patching OpenVPN

--- scripts/patch-openvpn.sh
+++ scripts/patch-openvpn.sh
@@ -34,9 +34,9 @@ cd tmp
 
 # Download OpenVPN
 echo "Downloading OpenVPN..."
-curl -fL https://raw.githubusercontent.com/OpenVPN/openvpn/master/COPYING --output "$ROOT_DIR/../share/openvpn/COPYING"
-curl -fL https://raw.githubusercontent.com/OpenVPN/openvpn/master/COPYRIGHT.GPL --output "$ROOT_DIR/../share/openvpn/COPYRIGHT.GPL"
-curl -fL https://swupdate.openvpn.org/community/releases/openvpn-$OPENVPN_VERSION.tar.gz --output openvpn-$OPENVPN_VERSION.tar.gz
+curl -fLsS https://raw.githubusercontent.com/OpenVPN/openvpn/master/COPYING --output "$ROOT_DIR/../share/openvpn/COPYING"
+curl -fLsS https://raw.githubusercontent.com/OpenVPN/openvpn/master/COPYRIGHT.GPL --output "$ROOT_DIR/../share/openvpn/COPYRIGHT.GPL"
+curl -fLsS https://swupdate.openvpn.org/community/releases/openvpn-$OPENVPN_VERSION.tar.gz --output openvpn-$OPENVPN_VERSION.tar.gz
 echo "13702526f687c18b2540c1a3f2e189187baaa65211edcf7ff6772fa69f0536cf  openvpn-$OPENVPN_VERSION.tar.gz" | sha256sum -c -
 echo "Decompressing OpenVPN..."
 tar -xf openvpn-$OPENVPN_VERSION.tar.gz
@@ -45,8 +45,8 @@ cd openvpn-$OPENVPN_VERSION || exit 1
 
 # Apply OpenVPN patch by 'samm-git'
 echo "Downloading OpenVPN Patch by 'samm-git'..."
-curl -fL https://raw.githubusercontent.com/samm-git/aws-vpn-client/master/LICENSE --output "$ROOT_DIR/../share/openvpn/PATCH-LICENSE"
-curl -fL https://raw.githubusercontent.com/samm-git/aws-vpn-client/master/openvpn-v2.6.12-aws.patch --output openvpn-v2.6.12-aws.patch
+curl -fLsS https://raw.githubusercontent.com/samm-git/aws-vpn-client/master/LICENSE --output "$ROOT_DIR/../share/openvpn/PATCH-LICENSE"
+curl -fLsS https://raw.githubusercontent.com/samm-git/aws-vpn-client/master/openvpn-v2.6.12-aws.patch --output openvpn-v2.6.12-aws.patch
 echo "d3d6831dd250af27c0e4fe1f9e820e157a0230d86b0c60ec21dae53266884bbf9dbc46ce2ba29c0f37efd640d2ccb614e81feaa1a66e238f2e259ff33472b2dc  openvpn-v2.6.12-aws.patch" | sha512sum -c -
 echo "Applying OpenVPN Patch by 'samm-git'..."
 patch -p1 <openvpn-v2.6.12-aws.patch
